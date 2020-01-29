8b881abf948c84de39e35d91618b2e4a8ef86cd2
Runo-saduwa <orunosaduwa@gmail.com>
2020-01-29 23:30:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import './App.css';
+import AddData from './AddData';
+import { Provider } from './Context';
+let constant = 4051.52;
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	// componentDidMount() {
+	// 	const { time, initPressure, dimTime } = this.state.formValues;
+	// 	let pressures = [ initPressure ];
+	// 	if (time === 0) {
+	// 		this.setState({
+	// 			results: [
+	// 				{
+	// 					meanPressure: 0,
+	// 					waterInflux: 0,
+	// 					cumWaterInflux: 0
+	// 				}
+	// 			],
+	// 			firstPressure: initPressure,
+	// 			pressures: pressures
+	// 		});
+	// 	} else if (time > 0 && this.state.pressures.length > 1 && this.state.pressures.length <= 3) {
+	// 		// console.log(this.state.pressures)
+
+	// 		let meanPressure = (this.state.firstPressure - initPressure) / 2;
+	// 		let pressures = [ ...this.state.pressures ];
+	// 		this.setState({ pressures: pressures.push(initPressure) });
+	// 		let halfCalc = constant * meanPressure;
+	// 		let halfCalculated = [ ...this.state.halfCalculated ];
+	// 		halfCalculated.push(halfCalc);
+	// 		let dimTimes = [ ...this.state.dimTimes ];
+	// 		dimTimes.push(this.state.formValues.dimTime);
+	// 		//console.log(dimTimes)
+	// 	}
+	// }
+
+	// onChange = (e) => {
+	// 	this.setState({
+	// 		formValues:{
+	// 			[e.target.name]: parseFloat(e.target.value)
+	// 		}
+	// 	})
+
+	//console.log(this.state.formValues.time)
+	//	}
+
+	// submitHandler = (formValues) => {
+	// 	//e.preventDefault();
+	// 	//event.preventDefault();
+	//    console.log(formValues.time, formValues.dimTime)
+	// }
+
+	render() {
+		return (
+			<Provider>
+				<Fragment>
+					<h1>Hello app</h1>
+					<AddData />
+				</Fragment>
+			</Provider>
+		);
+	}
 }
 
 export default App;
---
+++ src/Context.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+const constant = 4051.52;
+export const Context = React.createContext();
+
+export class Provider extends Component {
+	state = {
+		//this is for testing
+		//firstPressure: 4014,
+		//this is the original state
+		firstPressure: null,
+		// this is for testing
+		//pressures: [2, 33, 4],
+		//this is the original state
+		pressures: [],
+		halfCalculated: [],
+		dimTimes: [],
+		results: [
+			// {
+			// 	time: 0,
+			// 	initPressure: 4014,
+			// 	dimTime: 0,
+			// 	meanPressure: null,
+			// 	waterInflux: null,
+			// 	cumWaterInflux: null
+			// },
+			// {
+			// 	time: 0,
+			// 	initPressure: 4014,
+			// 	dimTime: 0,
+			// 	meanPressure: null,
+			// 	waterInflux: null,
+			// 	cumWaterInflux: null
+			// },
+			// {
+			// 	time: 0,
+			// 	initPressure: 4014,
+			// 	dimTime: 0,
+			// 	meanPressure: null,
+			// 	waterInflux: null,
+			// 	cumWaterInflux: null
+			// }
+		]
+	};
+
+	onSubmitHandler = (time, initPressure, dimTime, e) => {
+		e.preventDefault();
+		let pressures = [ ...this.state.pressures ];
+		pressures.push(initPressure);
+		this.setState({
+			pressures
+		});
+
+		if (time === 0) {
+			this.setState({
+				results: [
+					...this.state.results,
+					{
+						time,
+						initPressure,
+						dimTime,
+						meanPressure: 0,
+						waterInflux: 0,
+						cumWaterInflux: 0
+					}
+				],
+				firstPressure: initPressure
+				//pressures: pressures
+			});
+
+			//&& this.state.pressures.length > 1 && this.state.pressures.length <= 3
+		} else if (time > 0 && pressures.length > 1 && pressures.length <= 3) {
+			//CALCULATE THE MEAN PRESSURE
+			let meanPressure = (this.state.firstPressure - initPressure) / 2;
+			let halfCalc = constant * meanPressure;
+			let halfCalculated = [ ...this.state.halfCalculated ];
+			halfCalculated.push(halfCalc);
+			let dimTimes = [ ...this.state.dimTimes ];
+			dimTimes.push(dimTime);
+			let reversedDimTimes = dimTimes.reverse();
+			this.setState({
+				results: [
+					...this.state.results,
+					{
+						time,
+						initPressure,
+						dimTime,
+						cumWaterInflux: halfCalculated
+							.map((value, i) => {
+								return value * reversedDimTimes[i];
+							})
+							.reduce((prev, next) => {
+								return prev + next;
+							})
+					}
+				],
+				// firstPressure: initPressure,
+				pressures: pressures,
+				halfCalculated: halfCalculated,
+				dimTimes: dimTimes
+			});
+		} else if (time > 0 && pressures.length > 3) {
+			let meanPressure = (pressures[pressures.length - 3] - initPressure) / 2;
+			let halfCalc = constant * meanPressure;
+			let halfCalculated = [ ...this.state.halfCalculated ];
+			halfCalculated.push(halfCalc);
+			console.log(halfCalculated);
+			let dimTimes = [ ...this.state.dimTimes ];
+			dimTimes.push(dimTime);
+			//use sorting here
+			let reversedDimTimes = dimTimes.sort((a, b) => b - a);
+			console.log(reversedDimTimes);
+			this.setState({
+				results: [
+					...this.state.results,
+					{
+						time,
+						initPressure,
+						dimTime,
+						cumWaterInflux: halfCalculated
+							.map((value, i) => {
+								return value * reversedDimTimes[i];
+							})
+							.reduce((prev, next) => {
+								return prev + next;
+							})
+					}
+				],
+				// firstPressure: initPressure,
+				pressures: pressures,
+				halfCalculated: halfCalculated,
+				dimTimes: dimTimes
+			});
+			console.log(meanPressure);
+		}
+		//console.log(time, initPressure, dimTime, pressures);
+	};
+
+	render() {
+		return (
+			<Context.Provider value={{ ...this.state, onSubmitHandler: this.onSubmitHandler }}>
+				{this.props.children}
+			</Context.Provider>
+		);
+	}
+}
+
+export const Consumer = Context.Consumer;
