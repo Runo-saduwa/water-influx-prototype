5801153bc2513a6c78a71373e31dad9543b7d2f9
Runo-saduwa <orunosaduwa@gmail.com>
2020-01-31 22:48:46 +0100

    set up react-router

--- src/App.js
+++ src/App.js
@@ -1,65 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './App.css';
 import AddData from './AddData';
 import { Provider } from './Context';
-let constant = 4051.52;
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
 class App extends Component {
-	// componentDidMount() {
-	// 	const { time, initPressure, dimTime } = this.state.formValues;
-	// 	let pressures = [ initPressure ];
-	// 	if (time === 0) {
-	// 		this.setState({
-	// 			results: [
-	// 				{
-	// 					meanPressure: 0,
-	// 					waterInflux: 0,
-	// 					cumWaterInflux: 0
-	// 				}
-	// 			],
-	// 			firstPressure: initPressure,
-	// 			pressures: pressures
-	// 		});
-	// 	} else if (time > 0 && this.state.pressures.length > 1 && this.state.pressures.length <= 3) {
-	// 		// console.log(this.state.pressures)
-
-	// 		let meanPressure = (this.state.firstPressure - initPressure) / 2;
-	// 		let pressures = [ ...this.state.pressures ];
-	// 		this.setState({ pressures: pressures.push(initPressure) });
-	// 		let halfCalc = constant * meanPressure;
-	// 		let halfCalculated = [ ...this.state.halfCalculated ];
-	// 		halfCalculated.push(halfCalc);
-	// 		let dimTimes = [ ...this.state.dimTimes ];
-	// 		dimTimes.push(this.state.formValues.dimTime);
-	// 		//console.log(dimTimes)
-	// 	}
-	// }
-
-	// onChange = (e) => {
-	// 	this.setState({
-	// 		formValues:{
-	// 			[e.target.name]: parseFloat(e.target.value)
-	// 		}
-	// 	})
-
-	//console.log(this.state.formValues.time)
-	//	}
-
-	// submitHandler = (formValues) => {
-	// 	//e.preventDefault();
-	// 	//event.preventDefault();
-	//    console.log(formValues.time, formValues.dimTime)
-	// }
-
 	render() {
 		return (
-			<Provider>
-				<Fragment>
-					<h1>Hello app</h1>
-					<AddData />
-				</Fragment>
-			</Provider>
+			<Router>
+				 <h1>WATER INFLUX COMPUTATION SOFTWARE</h1>
+               <Switch>
+			    <Provider>
+					<Route exact path="/" component={AddData}/>
+                 </Provider>
+			   </Switch>
+			</Router>
 		);
 	}
 }
